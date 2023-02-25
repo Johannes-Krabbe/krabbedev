4690f0e9cff3be71a9445570560321eb3a7903a3
Johannes Krabbe <Johannes@Krabbe.net>
2023-02-25 21:33:33 +0100

    fix bug with unpublished posts

--- utils/getSlugs.ts
+++ utils/getSlugs.ts
@@ -1,4 +1,7 @@
 import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+
 import { sync } from "glob";
 
 const articlesPath = path.join(process.cwd(), "content/posts");
@@ -8,6 +11,11 @@ export async function getSlugs(): Promise<string[]> {
 
   return paths.map((path) => {
     // holds the paths to the directory of the article
+    const source = fs.readFileSync(path, "utf-8");
+    const { data } = matter(source);
+    if (data.published !== "true") {
+      return "";
+    }
     const pathContent = path.split("/");
     const fileName = pathContent[pathContent.length - 1];
     const [slug, _extension] = fileName.split(".");
